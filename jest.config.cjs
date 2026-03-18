d6a27bb71df0da9985e2c510af7a0470d90cbfa0
EthannSchneider <ethann@shkermit.ch>
2026-03-18 11:27:59 +0100

    feat(tests): update Jest configuration for ESM support and module name mapping

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,17 @@
 /* eslint-env node */
 module.exports = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
   clearMocks: true,
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+      tsconfig: "tsconfig.json",
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
